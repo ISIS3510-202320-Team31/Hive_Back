0ba32cd6972e8383d307ebf52790b67e1d963f35
luccasrojas <l.rojasb@uniandes.edu.co>
2023-09-15 22:53:43 -0500

    initial_back routing

--- event/views.py
+++ event/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Event
+from .logic.EventForm import EventForm
+import json
 
-# Create your views here.
+# JSON format, CRUD
+
+@csrf_exempt
+def index(request):
+    try:
+        if request.method == 'GET':
+            #events = Event.objects.all()
+            #data = serializers.serialize('json', events)
+            #return JsonResponse(data, safe=False)
+            return JsonResponse({'hola': 'si'})
+        if request.method == 'POST':
+            data = json.loads(request.body)
+    except Exception as e:
+        return JsonResponse({'error': str(e)})
+
--- hive_back/urls.py
+++ hive_back/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from event.views import index as event_index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('events/', event_index, name='events')
 ]
