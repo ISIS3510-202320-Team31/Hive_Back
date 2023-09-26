00a21c408a95b14f3e62c450fa1cea76af33f11a
Panis26 <ca.sanchezo1@uniandes.edu.co>
2023-09-25 20:11:02 -0500

    Add url request to weight

--- hive_back/urls.py
+++ hive_back/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from event.views import index_list as event_index_list, index_detail as event_index_detail, index_participants as event_index_participants
+from event.views import index_list as event_index_list, index_detail as event_index_detail, index_participants as event_index_participants, index_list_by_date as event_index_list_by_date
 from user.views import index_list as user_index_list, index_one as user_index_one
+from weight.views import index_list as weight_index_list, index_detail as weight_index_detail, index_user as weight_index_user, index_tag as weight_index_tag
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +26,7 @@ urlpatterns = [
     path('events/<str:pk>/participants/', event_index_participants, name='event_participants'),
     path('events/date/<str:date>/', event_index_list_by_date, name='event_list_by_date'),
     path('users/', user_index_list, name='user_list'),
-    path('users/<uuid:user_id>/', user_index_one, name='user_one')
+    path('users/<uuid:user_id>/', user_index_one, name='user_one'),
+    path('weights/', weight_index_list, name='weight_list'),
+    path('weights/<str:pk>/', weight_index_detail, name='weight_detail'),
 ]
