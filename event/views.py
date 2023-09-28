4291b7a5ec7164d6f1267f51a83350c6b6217bf4
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-09-27 23:41:38 -0500

    fixed date

--- event/views.py
+++ event/views.py
@@ -186,14 +186,37 @@ def index_participants(request, pk):
         return JsonResponse({'message': f'Participant {user_id} added successfully to event {pk}'})
 
 @csrf_exempt
-def index_list_by_date(request, date):
+def index_list_by_date(request, date):    
     if request.method == 'GET':
-        
-        #Get the eventes that match the date or are after the date given and order them by date
         events = Event.objects.filter(date__gte=date).order_by('date')
         event_data = list(events.values())
         
-                
+        # Add the creator name to the event
+        for event in event_data:
+            links_complete = []
+            tags_complete = []
+            participants_complete = []
+            event['creator'] = User.objects.get(pk=event['creator_id']).name
+            indiv_event = Event.objects.get(pk=event['id'])
+            
+            # Add the participants, tags and links to the event, but only their ids
+            # The frontend will have to make a request to get the data of each participant, tag and link
+            participants = list(indiv_event.participants.values())
+            tags = list(indiv_event.tags.values())
+            links = list(indiv_event.links.values())
+
+            for participant in participants:
+                participants_complete.append(participant['id'])
+            for tag in tags:
+                tags_complete.append(tag['name'])
+            for link in links:
+                links_complete.append(link['text'])
+            
+            event['participants'] = participants_complete
+            event['tags'] = tags_complete
+            event['links'] = links_complete
+
         return JsonResponse(event_data, safe=False, json_dumps_params={'indent': 4})
+        
     else:
         return JsonResponse({'message': 'The request must be a GET or POST'}, status=400)
